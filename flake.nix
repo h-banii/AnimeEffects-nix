b6509c71056ca626b00aa7f0c701310bd5421fd9
h-banii <121690516+h-banii@users.noreply.github.com>
2025-03-22 01:57:29 -0300

    initial commit

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "2D Animation Tool";
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      systems,
+      ...
+    }:
+    let
+      inherit (nixpkgs) lib;
+      forAllSystems = lib.genAttrs (import systems);
+      pkgsFor = forAllSystems (system: nixpkgs.legacyPackages.${system});
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = pkgsFor.${system};
+        in
+        {
+          default = self.packages.${system}.animeeffects; 
+          animeeffects = pkgs.callPackage ./package.nix { };
+        }
+      );
+    };
+}
